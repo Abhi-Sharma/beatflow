8876f0235ac07c459f5109d5177e51df2837efec
Abhi-Sharma <abhisharma.rediffmail@gmail.com>
2026-04-15 23:48:18 +0530

    some changes or fixes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.beatflow.app',
   appName: 'BeatFlow',
   webDir: 'public',
-  bundledWebRuntime: false,
   server: {
     url: 'https://beatflow.space',
     cleartext: false
